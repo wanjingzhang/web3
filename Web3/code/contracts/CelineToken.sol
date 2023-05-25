bddb20d813477184ba9dcad68019c97545e2996e
wanjingzhang <95616085@qq.com>
2023-05-25 16:46:35 +0800

    feat: 智能货币委托转账

--- Web3/code/contracts/CelineToken.sol
+++ Web3/code/contracts/CelineToken.sol
@@ -11,8 +11,9 @@ contract CelineToken {
     string public symbol = "CLT"; // 代币符号
     uint256 public decimals = 18; // 货币位数
     uint256 public totalSupply; //总供应量
-    // 自动生成getter方法
+    // 自动生成getter方法 余额
     mapping(address => uint256) public balanceOf; // mapping 返回某个账号对应的代币的数量
+    mapping(address => mapping(address => uint256)) public allowance;
 
     constructor() {
         // 部署账号
@@ -22,6 +23,12 @@ contract CelineToken {
 
     // 事件类型，在合约中一旦被触发，它就会被存储在交易的机制、日志中
     event Transfer(address indexed _from, address indexed _to, uint256 _value);
+
+    event Approval(
+        address indexed _owner,
+        address indexed _spender,
+        uint256 _value
+    );
 
     // 转账
     function transfer(
@@ -45,5 +52,43 @@ contract CelineToken {
 
         // 触发事件
         emit Transfer(_from, _to, _value);
+    }
+
+    // 授权给不同交易所的数额
+    function approve(
+        address _spender,
+        uint256 _value
+    ) public returns (bool success) {
+        // msg.sender 是当前授权的账号
+        // _spender 第三方交易所的账号地址
+        // _value 授权得钱数,
+        // require 如果这里出错了，那么我们消耗的gas值可以退回，后面的gas也都会退回
+        require(_spender != address(0));
+
+        allowance[msg.sender][_spender] = _value;
+        // 证明
+        emit Approval(msg.sender, _spender, _value);
+
+        return true;
+    }
+
+    // 被授权的交易所调用
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _value
+    ) public returns (bool success) {
+        // _from 某个放款账号
+        // _to 收款账号
+        // msg.sender 交易所的账号地址
+
+        // 账号钱数大于转账的钱数
+        require(balanceOf[_from] >= _value);
+
+        // 授权的需要这么多钱
+        require(allowance[_from][msg.sender] >= _value);
+        allowance[_from][msg.sender] = allowance[_from][msg.sender].sub(_value);
+        _transfer(_from, _to, _value);
+        return true;
     }
 }
--- Web3/code/scripts/test-transfer.js
+++ Web3/code/scripts/test-transfer.js
@@ -18,27 +18,27 @@ module.exports = async function (callback) {
   const celinetoken = await CelineToken.deployed();
   // 默认位
   let res1 = await celinetoken.balanceOf(
-    "0x10c4519D46F598F656C6cf49aB5b70aA3079b74A"
+    "0x71b8f4AB42aA31dD5C00B36150Dc1D47aAb3d3b7"
   );
 
   console.log(`第一个账号的值：${fromWei(res1)}`); // 100，0000
 
   // 转账10000Token币
   await celinetoken.transfer(
-    "0x4d0bbA09a808B2a5511594267dfBb630fC706787",
+    "0xf5bAb059D7B80640319922A96238832385EDBD40",
     toWei(10000),
     {
-      from: "0x10c4519D46F598F656C6cf49aB5b70aA3079b74A",
+      from: "0x71b8f4AB42aA31dD5C00B36150Dc1D47aAb3d3b7",
     }
   );
 
   let res3 = await celinetoken.balanceOf(
-    "0x4d0bbA09a808B2a5511594267dfBb630fC706787"
+    "0xf5bAb059D7B80640319922A96238832385EDBD40"
   );
   console.log(`第二个账号的值：${fromWei(res3)}`); // 1，0000
 
   let res2 = await celinetoken.balanceOf(
-    "0x10c4519D46F598F656C6cf49aB5b70aA3079b74A"
+    "0x71b8f4AB42aA31dD5C00B36150Dc1D47aAb3d3b7"
   );
   console.log(`转账完成后，第一个账号的值：${fromWei(res2)}`); // 99，0000
 
